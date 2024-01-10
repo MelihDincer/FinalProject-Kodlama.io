18c15d3201bdbd9e65a127fc03e7d3eee152adc6
Melih DİNÇER <m_dincer41@hotmail.com>
2024-01-10 12:15:48 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        //
+        private IProductDal _productDal;
+        //ProductManager newlendiğinde bana bir tane IProductDal referansı ver. Bugün entityframework ertesi gün docker olabilir. Hepsi aynı referansı tutacak sonuçta.
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Entities;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ProductId = 1, CategoryId = 1, ProductName = "Bardak", UnitPrice=15, UnitsInStock = 15},
+                new Product{ProductId = 1, CategoryId = 1, ProductName = "Kamera", UnitPrice=500, UnitsInStock = 3},
+                new Product{ProductId = 1, CategoryId = 1, ProductName = "Telefon", UnitPrice=1500, UnitsInStock = 2},
+                new Product{ProductId = 1, CategoryId = 1, ProductName = "Klavye", UnitPrice=150, UnitsInStock = 65},
+                new Product{ProductId = 1, CategoryId = 1, ProductName = "Fare", UnitPrice=85, UnitsInStock = 1},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ - Language Integrated Query (Dile Gömülü Sorgu)
+
+            //Linq bilmediğimizi varsayalım burada.
+            //Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if(p.ProductId == product.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+            //_products.Remove(productToDelete);
+
+            //Burada ise Linq bildiğimizi varsayalım.
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p=>p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul.
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId); //Ürünü bulduk.
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
