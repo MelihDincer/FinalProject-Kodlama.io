49756847a575f61dce38fcd93bfe8bde4b33b9b7
Melih DİNÇER <m_dincer41@hotmail.com>
2024-01-18 13:31:24 +0300

    11.Gün Ders sonu.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -14,18 +14,44 @@ namespace WebAPI.Controllers
         //Loosely coupled --> Gevşek bağlılık
         //Fieldların defaultu private dır.
         //IoC Container -- Inversion of control --> Değişimin kontolü
-        IProductService _productService; 
+        IProductService _productService;
 
         public ProductsController(IProductService productService)
         {
             _productService = productService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             var result = _productService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Post(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
